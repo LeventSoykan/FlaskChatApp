186b1c39aa457ac4d5c3a4c8661fc18d1365ddbb
LeventSoykan <levent.soykan@turkcell.com.tr>
2022-07-24 18:04:26 +0300

    Add leave room event

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, join_room, leave_room
 
 app = Flask(__name__)
-
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
@@ -14,10 +15,29 @@ def chat():
     room = request.args.get('room')
 
     if username and room:
-        return render_template('chat.html')
+        return render_template('chat.html', room=room, username=username)
     else:
         return redirect(url_for('home'))
 
 
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    app.logger.info(f"{data['username']} has send message to the room {data['room']}: {data['message']}")
+    socketio.emit('receive_message', data, room=data['room'])
+
+
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    app.logger.info(f"{data['username']} has joined the room {data['room']}")
+    join_room(data['room'])
+    socketio.emit('join_room_announcement', data)
+
+
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+    app.logger.info(f"{data['username']} has left the room {data['room']}")
+    leave_room(data['room'])
+    socketio.emit('leave_room_announcement', data)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
